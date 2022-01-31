0bc3fd8c1ec5341449e611c3354d7689146e6799
Gandhyman <gandhycoder@gmail.com>
2022-01-31 12:38:12 -0700

    Corregido sequelize.js nombre bd

--- api/src/database/sequelize.js
+++ api/src/database/sequelize.js
@@ -6,7 +6,7 @@ const {
     DB_USER, DB_PASSWORD, DB_HOST,
 } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/pokemon`, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/ecommerce`, { // ecommerce es el nombre de la base de datos local
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
     freezeTableName: true //prevent sequelize from pluralizing table names
