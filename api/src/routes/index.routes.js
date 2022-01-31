d241bc1ab8c6f3e6ebb078cfbc02b00e212680da
JoseGarcia <josernestogarcia609@gmail.com>
2022-01-31 16:59:47 -0300

    jose-Add-login.routes.js -- login.routes.controller.js

--- api/src/routes/index.routes.js
+++ api/src/routes/index.routes.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const home = require('./home.routes');
 
+const login = require('./login.routes');    
+
+
+
+
+
 
 
 const routes = (server) => {
 
     server.use('/', home);
-}
+    
+    server.use('/login', login);
+
+
+
+
+};
 
 
 
