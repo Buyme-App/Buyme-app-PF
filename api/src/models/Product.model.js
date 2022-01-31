544a91bbfd24dd166715d18deba0c0b9e89eda26
Gandhyman <gandhycoder@gmail.com>
2022-01-31 12:24:24 -0700

    Creado modelo de productos

---
+++ api/src/models/Product.model.js
@@ -0,0 +1,40 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+    // defino el modelo
+    sequelize.define('product', {
+
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            allowNull: false,
+            primaryKey: true,
+        },
+
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        price: {
+            type: DataTypes.INTEGER,
+            defaultValue: ''
+        },
+
+        idCategory: {
+            type: DataTypes.INTEGER,
+            defaultValue: '50'
+        },
+
+        favorite: {
+            type: DataTypes.INTEGER,
+            defaultValue: '50'
+        }
+    },
+        {
+            timestamps: false,
+            freezeTableName: true,
+        }
+    );
+};
