7dd624cd1a874e2e2953be1db0f167cb5ba9e000
Victor Rueda <vialruma1985@gmail.com>
2022-02-01 10:54:05 -0500

    Se agregan la ruta createProduct.routes y el controlador createProduct.controller para crear un producto.

---
+++ api/src/controllers/createProduct.controller.js
@@ -0,0 +1,24 @@
+const {product} = require('../database/db');
+const showErrors = require('../messageConsole');
+
+// Funcion controladora para crear un producto en la base de datos.
+async function createProduct(name, price, favorite){
+
+    try {
+         const createProduct = await product.create({
+             name,
+             price,
+             favorite
+         });
+         return createProduct;
+
+    } catch (error) {
+       showErrors('createProduct', error)
+       return 500
+    }
+
+};
+
+
+
+module.exports = createProduct;
