84f3006f75e733760abca47ef0116a599dad34cc
nicolius888 <nicola.edgardo@gmail.com>
2022-02-01 12:20:23 -0300

    refact(models): modificacion del require y del define

---
+++ api/src/models/SubCategories.model.js
@@ -0,0 +1,15 @@
+const { DataTypes } = require("sequelize");
+module.exports = (sequelize) => {
+  sequelize.define("Sub-Categories", {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+};
