91aff332fbe45f7c322215dc2565c5317e289f39
Gandhyman <gandhycoder@gmail.com>
2022-02-01 09:13:01 -0700

    Update index.routes.js
    Agregue dos lineas que faltaban de la version mas reciente

--- api/src/routes/index.routes.js
+++ api/src/routes/index.routes.js
@@ -1,33 +1,14 @@
 const express = require('express');
 const home = require('./home.routes');
-
+const routerGetAdmin = require('../routes/getDataAmin.routes.js'); //Agregue esta linea 
 const login = require('./login.routes');    
-
-
-
-
-
-
 
 const routes = (server) => {
 
-    server.use('/', home);
-    
+    server.use('/', home);  
+    server.use('/getDataAdmin', routerGetAdmin); //Agregue esta linea
     server.use('/login', login);
 
-
-
-
 };
-
-
-
-
-
-
-
-
-
-
 
 module.exports = routes;
