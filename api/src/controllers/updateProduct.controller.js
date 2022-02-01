216f34864aae681852b57e409ee5184a0fca2b74
Victor Rueda <vialruma1985@gmail.com>
2022-02-01 12:24:38 -0500

    Se agregan la ruta updateProduct.routes y el controlador updateProduct.controller para actualizar un producto.

---
+++ api/src/controllers/updateProduct.controller.js
@@ -0,0 +1,27 @@
+const {product} = require('../database/db');
+const showErrors = require('../messageConsole');
+
+async function updateProduct(id, name, price, favorite){
+
+    try {
+        
+        const updateItem = await product.update({
+            name,
+            price,
+            favorite
+        },
+        {where: {id: parseInt(id)}}
+        );
+
+        if(updateItem > 0) return true;
+        return false;
+
+    } catch (error) {
+        
+        showErrors('updateProduct', error);
+        return 500;
+    }
+};
+
+
+module.exports = updateProduct;
