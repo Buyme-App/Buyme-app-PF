fef80159dd28aa720085cc1f9e22346b3ada431a
Nicolius888 <82170757+Nicolius888@users.noreply.github.com>
2022-02-02 14:49:11 -0800

    Update createInvoice.routes.js

--- api/src/routes/createInvoice.routes.js
+++ api/src/routes/createInvoice.routes.js
@@ -3,7 +3,7 @@ const createInvoice = require("../controllers/createInvoice.controller");
 const showErrors = require("../messageConsole");
 const router = Router();
 
-// Ruta /createProduct que envía los datos recibidos por body al controlador createProduct
+// Ruta /createInvoice que envía los datos recibidos por body al controlador createInvoice
 // y responde con la data del producto creado o un error 500.
 router.post("/", async (req, res) => {
   const { products } = req.body;
