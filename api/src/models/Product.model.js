e9521dbf5407d2e2562383903a6225084c88c776
Gandhyman <gandhycoder@gmail.com>
2022-02-02 07:47:59 -0700

    Add: More fields in Product

--- api/src/models/Product.model.js
+++ api/src/models/Product.model.js
@@ -4,25 +4,71 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
     // defino el modelo
     sequelize.define("product", {
-
         id: {
             type: DataTypes.UUID,
             defaultValue: DataTypes.UUIDV4,
             allowNull: false,
             primaryKey: true,
         },
-
         name: {
             type: DataTypes.STRING,
             allowNull: false,
             unique: true
         },
+        maker: { //fabricante
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        model: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        SKU: {
+            type: DataTypes.INTEGER,
+            allowNull: true
+        },
         price: {
             type: DataTypes.INTEGER,
+            allowNull: true
         },
-
+        offerPrice:{
+            type: DataTypes.INTEGER,
+            allowNull: true
+        },
+        stock: {
+            type: DataTypes.INTEGER,
+            allowNull: true
+        },
+        inventary: {
+            type: DataTypes.INTEGER,
+            allowNull: true
+        },
         favorite: {
             type: DataTypes.BOOLEAN,
-        }
+            allowNull: true
+        },
+        featured:{
+            type: DataTypes.BOOLEAN,
+            allowNull: true
+        },
+        paused:{
+            type: DataTypes.BOOLEAN,
+            allowNull: true
+        },
     });
 };
+
+// Nombre
+// Modelo
+// Marca
+// Fabricante
+// SKU o (número de artículo)
+// Precio
+// Precio oferta
+// Producto destacado (sí / no)
+// Descripción
+// Stock
