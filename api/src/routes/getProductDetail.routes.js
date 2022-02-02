a9da3e5c919a7896b833ca56af3332848a417785
Gandhyman <gandhycoder@gmail.com>
2022-02-02 11:01:09 -0700

    Fixed: getProductDetail errors

--- api/src/routes/getProductDetail.routes.js
+++ api/src/routes/getProductDetail.routes.js
@@ -2,8 +2,9 @@ const {Router}= require('express');
 const router = Router();
 const getProductDetail = require ('../controllers/getProductDetail.controller')
 
-
-router.get('/productDetail/:idProduct/:nameProduct', getProductDetail());                            //por query
-
-
+router.get('/productDetail/:idProduct/:nameProduct', async (req, res) => {
+    getProductDetail(req)  //por query
+})
+                     
 module.exports=router;
+// Fixed
