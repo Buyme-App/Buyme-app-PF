5d039d1e7be1b5f986a87e064606fe3cb97ee9c4
Victor Rueda <vialruma1985@gmail.com>
2022-02-02 18:54:01 -0500

    Se modifican los archivos del CRUD de productos para agregar nuevos campos y se modifican los archivo de getAdminData y cambian a getAllUsers, se agregan las rutas al controlador de rutas.

---
+++ api/src/routes/getAllUsers.routes.js
@@ -0,0 +1,24 @@
+const { Router } = require('express');
+const getAllUsers = require('../controllers/getAllUsers.controller');
+const showErrors = require('../messageConsole');
+const router = Router();
+
+// Ruta para ob
+router.get('/', async (req, res) => {
+
+    try {
+        const usersData = await getAllUsers();
+        if(usersData) return res.status(200).json(usersData);
+        else return res.status(404).send('There are no users in the database')
+
+    } catch (error) {
+        showErrors('/getDataAdmin', error)
+        res.status(404)
+    }
+});
+
+
+
+
+
+module.exports = router;
--- api/src/routes/index.routes.js
+++ api/src/routes/index.routes.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const home = require('./home.routes');
-const routerGetAdmin = require('../routes/getDataAmin.routes.js');
+const routerGetAllUsers = require('./getAllUsers.routes');
 const login = require('./login.routes');
 const routerGetProductDetail = require('../routes/getProductDetail.routes');
+const routerCreatePro = require('./createProduct.routes');
+const routerUpdatePro = require('./updateProduct.routes');
+const routerDeletePro = require('./deleteProduct.routes');
+const routerGetProducts = require ('./getAllProducts.routes.js');
+
 
 const routes = (server) => {
     server.use('/', home);
-    server.use('/getDataAdmin', routerGetAdmin);
+    server.use('/getAllUsers', routerGetAllUsers);
     server.use('/login', login);
-    server.use('/productDetail/', routerGetProductDetail );
+    server.use('/productDetail', routerGetProductDetail );
+    server.use('/createProduct', routerCreatePro);
+    server.use('/updateProduct', routerUpdatePro);
+    server.use('/deleteProduct', routerDeletePro);
+    server.use('/getAllProducts',routerGetProducts);
+
 }
 
 module.exports = routes;
