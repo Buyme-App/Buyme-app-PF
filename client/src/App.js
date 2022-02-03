ef962cc0a0f62e8d0a92f6ee6458e981f698f9b5
laubrener <79546818+laubrener@users.noreply.github.com>
2022-02-03 14:36:11 -0300

    agrego ruta de account, changeEmail y changePass

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,11 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./components/Login/Login";
 import PassRecovery from "./components/PassRecovery/PassRecovery";
 import AdminHome from "./components/AdminHome/AdminHome";
+import Account from "./components/Account/Account";
 import Aside from "./components/asideDashboard/asideDashboard";
+import AddUser from "./components/AddUsers/AddUser";
+import ChangeEmail from "./components/ChangeEmail/ChangeEmail";
+import ChangePassword from './components/ChangePassword/ChangePassword.jsx';
 
 function App() {
   return (
@@ -13,7 +17,11 @@ function App() {
         <Routes>
           <Route exact path="/admin" element={<Login />} />
           <Route exact path="/admin/home" element={<AdminHome />} />
-          <Route exact path="/admin/home/account" element={<Aside />} />
+          <Route exact path="/admin/home/aside" element={<Aside />} />
+          <Route exact path="/admin/home/account" element={<Account />} />
+          <Route exact path="/admin/home/addUser" element={<AddUser />} />
+          <Route exact path="/admin/home/changeEmail" element={<ChangeEmail />} />
+          <Route exact path="/admin/home/changePassword" element={<ChangePassword />} />
           <Route path="/admin/restorePassword" element={<PassRecovery />} />
         </Routes>
       </div>
