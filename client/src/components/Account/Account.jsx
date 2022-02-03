5a4ca5af944342c21c04989f6472d82cae8e7c41
laubrener <79546818+laubrener@users.noreply.github.com>
2022-02-03 14:34:58 -0300

    agrego rutas y css alaccount

--- client/src/components/Account/Account.jsx
+++ client/src/components/Account/Account.jsx
@@ -1,19 +1,20 @@
 import React from "react";
-import Aside from "../asideDashboard/asideDashboard";
 import styles from './Account.module.css';
 import {MdOutlineDelete} from 'react-icons/md';
+import { Link } from "react-router-dom";
 
 export default function Account (){
 
     return (
         <div className={styles.page}>
-            <div><Aside></Aside></div>
             <h1 className={styles.title}>Account</h1>
+            <h2>My Account</h2>
             <div className={styles.box}>
-                <button className={styles.button} type="submit">Change Email</button>
-                <button className={styles.button} type="submit">Change Password</button>
-                <button className={styles.button} type="submit">Add Users</button>
+                <Link to='/admin/home/changeEmail'><button className={styles.button} type="submit">Change Email</button></Link>
+                <Link to='/admin/home/changePassword'><button className={styles.button} type="submit">Change Password</button></Link>
+                <Link to='/admin/home/addUser'><button className={styles.button} type="submit">Add User</button></Link>
             </div>
+            <h2>Users</h2>
             <div><table style={{'marginBottom': '10%'}}>
                     <tr className={styles.chart}>
                         <td>ID</td>
@@ -24,19 +25,19 @@ export default function Account (){
                         <td>1</td>
                         <td>User1</td>
                         <td>user1@gmail.com</td>
-                        <button className={styles.x} type="submit">{MdOutlineDelete}</button>
+                        <button className={styles.delete} type="submit">{MdOutlineDelete}x</button>
                     </tr>
                     <tr className={styles.details}>
                         <td>2</td>
                         <td>User2</td>
                         <td>user2@gmail.com</td>
-                        <button className={styles.x} type="submit">{MdOutlineDelete}</button>
+                        <button className={styles.delete} type="submit">{MdOutlineDelete}x</button>
                     </tr>
                     <tr className={styles.details}>
                         <td>3</td>
                         <td>User3</td>
                         <td>user3@gmail.com</td>
-                        <button className={styles.x} type="submit">{MdOutlineDelete}</button>
+                        <button className={styles.delete} type="submit">{MdOutlineDelete}x</button>
                     </tr>
                 </table>
             </div>
