7980b8d51fa7ab69e8949747f3a488862fe9abf4
falewilli <alejandro.willi@gmail.com>
2022-02-03 19:50:21 -0300

    Added components & Home as default page

--- client/src/components/AdminHome/AdminHome.jsx
+++ client/src/components/AdminHome/AdminHome.jsx
@@ -1,36 +1,39 @@
 import React from "react";
 import Aside from "../asideDashboard/asideDashboard";
+import AdminDashboard from "../AdminDashboard/AdminDashboard";
 import Sales from "../Sales/Sales";
+import AdminNewProduct from "../AdminNewProduct/AdminNewProduct";
+import AdminProducts from "../AdminProducts/AdminProducts";
+import Queries from "../Queries/Queries";
+import Account from "../Account/Account";
 import style from "./adminHome.module.css";
-
 
 export default function AdminHome() {
   const [panelActive, setPanelActive] = React.useState({
-    // home: false,
-    // sales: false,
-    // newProduct: false,
-    // products: false,
-    // categories: false,
-    // customers: false,
-    // queries: false,
-    // account: false
-  })
+    home: true,
+    sales: false,
+    newProduct: false,
+    products: false,
+    categories: false,
+    customers: false,
+    queries: false,
+    account: false
+  });
   return (
     <div className={style.main_box}>
       <h1>Dashboard Home</h1> <br />
       <div className={style.main_container}>
-        <Aside setPanelActive={setPanelActive} panelActive={panelActive}/>
+        <Aside setPanelActive={setPanelActive} panelActive={panelActive} />
         <section>
-          <span>renderizado de contenido</span>
+          {/* <span>renderizado de contenido</span> */}
+          {panelActive.home && <AdminDashboard/>}
           {panelActive.sales && <Sales/>}
-          {panelActive.newProduct &&  <h1>new product section</h1>}
-          {panelActive.products && <h1>products section</h1>}
+          {panelActive.newProduct && <AdminNewProduct/>}
+          {panelActive.products && <AdminProducts/>}
           {panelActive.categories && <h1>Categories section</h1>}
-          {panelActive.customers && <h1>customers section</h1>}
-          {panelActive.queries && <h1>queries section</h1>}
-          {panelActive.account && <h1>account section</h1>}
-          
-
+          {panelActive.customers && <h1>Customers section</h1>}
+          {panelActive.queries && <Queries />}
+          {panelActive.account && <Account/>}
         </section>
       </div>
     </div>
