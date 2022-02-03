6cfc532f63e4a2749c2ba156db33e62e6008c32e
laubrener <79546818+laubrener@users.noreply.github.com>
2022-02-03 15:05:34 -0300

    elimine rutas

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./components/Login/Login";
 import PassRecovery from "./components/PassRecovery/PassRecovery";
 import AdminHome from "./components/AdminHome/AdminHome";
-import Account from "./components/Account/Account";
-import Aside from "./components/asideDashboard/asideDashboard";
-import AddUser from "./components/AddUsers/AddUser";
-import ChangeEmail from "./components/ChangeEmail/ChangeEmail";
-import ChangePassword from './components/ChangePassword/ChangePassword.jsx';
 
 function App() {
   return (
