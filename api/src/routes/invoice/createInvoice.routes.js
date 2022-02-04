3334fc4bc4b94e4d44cc209936d7b3d9a2752855
nicolius888 <nicola.edgardo@gmail.com>
2022-02-04 13:14:25 -0300

    fix(controller + route): cambia el formato de la factura

--- api/src/routes/invoice/createInvoice.routes.js
+++ api/src/routes/invoice/createInvoice.routes.js
@@ -6,11 +6,11 @@ const router = Router();
 // Ruta /createInvoice que envía los datos recibidos por body al controlador createInvoice
 // y responde con la data del producto creado o un error 500.
 router.post("/", async (req, res) => {
-  const { products } = req.body;
+  const { products, total } = req.body;
 
-  if (products) {
+  if (products && total) {
     try {
-      const create = await createInvoice(products);
+      const create = await createInvoice(products, total);
       res.json({ message: "Invoice created", data: create });
     } catch (error) {
       showErrors("post/createInvoice", error);
