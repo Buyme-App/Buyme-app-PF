234a7b3ec15323739ba6bef7d6d9de9ea9263684
Lucas <lucasferreiraok@outlook.com>
2022-02-04 15:01:24 -0300

    fixed (Login): validator

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -1,4 +1,8 @@
 import axios from "axios";
+
+const LOGIN_ADMIN = "LOGIN_ADMIN";
 // export const ACTION = "ACTION";
 //estos son ejemplos
 export const firstAction = () => {};
+
+export const loginAdmin = () => {};
