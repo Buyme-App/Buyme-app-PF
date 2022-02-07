22a238210aeb1a3d465033db143ade525fa2a430
nicolius888 <nicola.edgardo@gmail.com>
2022-02-07 18:52:57 -0300

    feat(models): Agrega modelo de cliente

---
+++ api/src/models/Client.model.js
@@ -0,0 +1,39 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define("client", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+    },
+    country: {
+      type: DataTypes.STRING,
+    },
+    phone: {
+      type: DataTypes.INTEGER,
+    },
+    address: {
+      type: DataTypes.STRING,
+    },
+    favorites: {
+      type: DataTypes.ARRAY(DataTypes.INTEGER),
+    },
+  });
+};
