fa5cc8b5c84a21859092bcdefa8c18168ab29762
Victor Rueda <vialruma1985@gmail.com>
2022-02-07 17:03:32 -0500

    Update Client.model.js

--- api/src/models/Client.model.js
+++ api/src/models/Client.model.js
@@ -7,6 +7,7 @@ module.exports = (sequelize) => {
       type: DataTypes.INTEGER,
       primaryKey: true,
       autoIncrement: true,
+      allowNull: false,
     },
     firstName: {
       type: DataTypes.STRING,
