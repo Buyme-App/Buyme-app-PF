ce0da82fb9c3a8a0663de6f8161d0954305a6fe1
nicolius888 <nicola.edgardo@gmail.com>
2022-02-09 18:34:48 -0300

    fix(models): Cambia el type de image a Array de .TEXT

--- api/src/models/Product.model.js
+++ api/src/models/Product.model.js
@@ -23,7 +23,7 @@ module.exports = (sequelize) => {
       defaultValue: 0, //Added
     },
     image: {
-      type: DataTypes.ARRAY(DataTypes.STRING),
+      type: DataTypes.ARRAY(DataTypes.TEXT),
       allowNull: true,
     },
     maker: {
