1f3cd216e9afd85f9d2d32d6fb1d486ce38d649a
falewilli <alejandro.willi@gmail.com>
2022-02-12 16:02:06 -0300

    Searchbar, reducers & actions

--- client/src/components/User/SearchBar/SearchBar.jsx
+++ client/src/components/User/SearchBar/SearchBar.jsx
@@ -1,39 +1,45 @@
-import React from 'react';
-import { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import styles from './SearchBar.module.css';
-import { BsSearch } from 'react-icons/bs';
+import React from "react";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { getProductsByName } from "../../../redux/actions";
+import styles from "./SearchBar.module.css";
+import { BsSearch } from "react-icons/bs";
 
-export default function SearchBar () {
+export default function SearchBar() {
   const dispatch = useDispatch();
-  const [name, setName] = useState('');
+  const [name, setName] = useState("");
 
-//   function handleInputChange(e){
-//       e.preventDefault();
-//       setName(e.target.value); // EL VALUE DEL STATE VA A TOMAR EL VALUE DEL INPUT
-//   }
+  function handleInputChange(e) {
+    e.preventDefault();
+    setName(e.target.value);
+  }
 
-//   function handleSubmit(e) {
-//     e.preventDefault();
-//     if (!name) {
-//       return alert('Please enter a name to start the search');
-//     } else {
-//       dispatch(getNamePokemons(name));  
-//       setName('');
-//     }
-//   }
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!name) {
+      return alert("Please enter a name to start the search");
+    } else {
+      dispatch(getProductsByName(name));
+      setName("");
+    }
+  }
 
   return (
     <div className={styles.search}>
       <input
-      type='text'
-      // value={name}
-      className={styles.input}
-      placeholder='Search by product name...'
-      // onChange={(e) => handleInputChange(e)}
+        type="text"
+        value={name}
+        className={styles.input}
+        placeholder="Search by product name or part of it..."
+        onChange={(e) => handleInputChange(e)}
       />
-      {/* <button className={styles.go} type='submit' onClick={(e) => handleSubmit(e)}>GO!</button> */}
-      <button className={styles.go} type='submit'><BsSearch /></button>
+      <button
+        className={styles.go}
+        type="submit"
+        onClick={(e) => handleSubmit(e)}
+      >
+        <BsSearch />
+      </button>
     </div>
-  )
+  );
 }
