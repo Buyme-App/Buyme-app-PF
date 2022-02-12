d47bc09adef050fd0dc7743ab3b92427c0e3b329
falewilli <alejandro.willi@gmail.com>
2022-02-12 09:34:34 -0300

    Fixes render when missing img

--- client/src/components/User/Cards/Cards.jsx
+++ client/src/components/User/Cards/Cards.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
-import styles from './Cards.module.css';
+import React from "react";
+import styles from "./Cards.module.css";
+import noimage from "../../../assets/imgdft.png";
 
-export default function Cards({name, price,image}){
-    return(
-
-        <div className={styles.Card}>       
-            <img className={styles.imge} src = {image} alt= 'img not found' width= "195px" height='170px'/>
-            <h3 className={styles.name}>{name}</h3>
-            <h5 className={styles.price}>$ {price}</h5>
-        </div>
-    )
+export default function Cards({ name, price, image }) {
+  return (
+    <div className={styles.Card}>
+      {/* <img src={image ? image : noimage} className={styles.imge} alt='img not found' width= "195px" height='170px'/> */}
+      {image.length ? (
+        <img
+          src={`${image[0]}`}
+          alt={`${name}`}
+          className={styles.imge}
+          width={`195px`}
+          height={`170px`}
+        />
+      ) : (
+        <img src={noimage} alt="not found" width="195px" height={`170px`} />
+      )}
+      <h3 className={styles.name}>{name}</h3>
+      <h5 className={styles.price}>$ {price}</h5>
+    </div>
+  );
 }
