569cd808720abe4ca95166f8bb5e38e03879503a
laubrener <79546818+laubrener@users.noreply.github.com>
2022-02-16 16:40:15 -0300

    google login creates new clients

--- client/src/components/User/GoogleLogin/GoogleLogin.jsx
+++ client/src/components/User/GoogleLogin/GoogleLogin.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { GoogleLogin, GoogleLogout } from 'react-google-login';
 import { useDispatch } from 'react-redux';
-import {LoginClientGoogle} from '../../../middlewares/LoginWithGoogle';
+import { createCustomer } from '../../../redux/actions';
 
 // Id del usuario en la google console developers.
 const clientId = "532893426828-r97cch5c0jid27g5ub373cu4n8kdo3qb.apps.googleusercontent.com";
@@ -9,22 +9,29 @@ const clientId = "532893426828-r97cch5c0jid27g5ub373cu4n8kdo3qb.apps.googleuserc
 
 
 
-export function LoginGoogle(props) {
+export function LoginGoogle(userData) {
 
     const dispatch = useDispatch();
 
     // Funcion onLoginSuccess que recibe los datos del login, los guarda en la variable y
-    const onLoginSuccess = (res) => {
-        
-        console.log('Success login with Google');
-        console.log(res);
+    const onLoginSuccess = async (res) => {
+
+        console.log('Success login with Google', res.profileObj);
 
         //Variable donde se almacenan los datos que google retorna.
         let  data = res.profileObj;
 
-        LoginClientGoogle(data);
+        const {givenName, familyName, email, googleId} =  await data;
 
-        
+        let obj = {
+            // Informacion a enviar al back.
+            firstName: givenName,
+            lastName: familyName,
+            email: email,
+            googleId: googleId
+        }
+
+        dispatch(createCustomer(obj));
     };
 
     // Funcion onLoginFailire que recibe el error si no se pudo hacer login con google.
@@ -32,11 +39,9 @@ export function LoginGoogle(props) {
 
         console.log('Login Failed:', res);
 
-        let  data = res.profileObj;
-
-        LoginClientGoogle(data);
     };
         
+    
     return (
         <div>
             
