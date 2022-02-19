507878f2f18eb67dd6727a8abc44ac6807cb3a83
zineR1 <agusdalvit@gmail.com>
2022-02-18 23:05:10 -0300

    FinishedCart3

--- client/src/components/User/Cart/Cart.jsx
+++ client/src/components/User/Cart/Cart.jsx
@@ -1,13 +1,47 @@
-import React from "react";
-import { useSelector, useDispatch } from "react-redux";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch} from "react-redux";
 import s from "./cart.module.css";
 import Item from "./Item/Item";
 import {Link} from "react-router-dom";
-import {CLEAR_CART, REMOVE_ALL_FROM_CART, REMOVE_ONE_FROM_CART, ADD_ONE_TO_CART} from "../../../redux/actions/index.js"
+import {CLEAR_CART, REMOVE_ALL_FROM_CART, REMOVE_ONE_FROM_CART, ADD_ONE_TO_CART,FILL_CART} from "../../../redux/actions/index.js"
 
 export default function Cart(props) {
+
   const cartState = useSelector((state) => state.cart);
   const dispatch = useDispatch();
+
+  // const[changeState, setChangeState] = useState({})
+  
+ 
+  useEffect(() => {
+  if(cartState.length === 0){
+     dispatch({type:FILL_CART})
+  }
+}, [dispatch])
+ 
+
+
+ useEffect(() => {
+   if(cartState.length > 0){
+   localStorage.setItem("cart", JSON.stringify(cartState))
+   }
+ }, [cartState])
+
+//  useEffect(() => {
+//   let data= localStorage.getItem("cart")
+//   if(data){
+//    setChangeState(JSON.parse(data))
+//   }
+// }, [cartState])
+
+  // const cartState = JSON.parse(result);
+
+  // if(cartState.length === 0){
+  //  dispatch({
+  //        type: FILL_CART,
+  //        payload:[]
+  //  })
+  // }
 
   const addToCart = (id) => {
        dispatch({
@@ -16,7 +50,6 @@ export default function Cart(props) {
        })
       }
     
-
   const delFromCart = (id, all= false) => {
    if(all){
      dispatch({
@@ -35,7 +68,7 @@ export default function Cart(props) {
     dispatch({type:CLEAR_CART})
   }
   
-  return props.trigger ? (
+  return props.trigger? (
     <div className={s.main_box}>
       <div className={s.cart_div}>
         <div className={s.close} onClick={() => props.setTrigger(false)}>
@@ -53,16 +86,18 @@ export default function Cart(props) {
               img={item.img}
               price={item.price}
               amount={item.amount}
+              stock={item.stock}
               delFromCart={delFromCart}
               addToCart={addToCart}
             /> 
           ))
         )}
-        {cartState.length? (
+        {cartState.length?(
         <button className={s.btn1}>BUY NOW</button>):
         (  <Link to="/shop"><button className={s.btn1}>GO SHOPPING</button></Link>)}
         <div>
         <button className={s.btn2}onClick={clearCart}>Clear Cart</button>
+        {/* <button onClick={fillingCart}>Fill Cart</button> */}
         </div>
           
       </div>
