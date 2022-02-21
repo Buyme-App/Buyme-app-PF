8d6c344145b95ac2d810c3225f64a0a4655dccab
Gandhyman <gandhycoder@gmail.com>
2022-02-20 18:12:49 -0700

    Warnings de /index eliminados

--- client/src/index.js
+++ client/src/index.js
@@ -12,7 +12,7 @@ import store from "./redux/store";
 //   ? process.env.REACT_APP_API
 //   : "http://localhost:3001";
 
-// axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
+//  axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
 
 ReactDOM.render(
   <Provider store={store}>
