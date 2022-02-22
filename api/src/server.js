e6ad9a7d0c7aaf875800f4261b190d9bbb5122a3
nicolius888 <nicola.edgardo@gmail.com>
2022-02-22 18:18:04 -0300

    whitelist

--- api/src/server.js
+++ api/src/server.js
@@ -5,15 +5,22 @@ const bodyParser = require("body-parser");
 const server = express();
 const cors = require("cors");
 
+const whiteList = [
+  "https://buyme-app-pf.vercel.app/",
+  "https://api.mercadopago.com/",
+  "https://api.mercadolibre.com/",
+];
+
 // Middlewares
 server.use(bodyParser.json({ limit: "10mb" }));
 server.use(express.json());
 server.use(morgan("dev"));
-server.use(cors());
+
+server.use(cors({ origin: whiteList }));
 
 // server.use((req, res, next) => {
 //   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Credentials", "true");
+//   res.header("Access-Control-Allow-Credentials", "");
 //   res.header(
 //     "Access-Control-Allow-Headers",
 //     "Origin, X-Requested-With, Content-Type, Accept"
