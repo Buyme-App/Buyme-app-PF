959446900e67527a6a7f222bcc834d8f7f281d9d
nicolius888 <nicola.edgardo@gmail.com>
2022-02-22 17:54:40 -0300

    another cors test

--- api/src/server.js
+++ api/src/server.js
@@ -3,22 +3,24 @@ const morgan = require("morgan");
 const router = require("./routes/index.routes");
 const bodyParser = require("body-parser");
 const server = express();
+const cors = require("cors");
 
 // Middlewares
 server.use(bodyParser.json({ limit: "10mb" }));
 server.use(express.json());
 server.use(morgan("dev"));
+server.use(cors());
 
-server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-});
+// server.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+//   next();
+// });
 
 // Routes
 router(server);
